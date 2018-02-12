aa99c88b48ae1cf6a18166eb3cd74a03df0ff27b
Andrei-Rosu <jamaika80@yahoo.com>
2018-02-12 17:36:36 +0100

    Wordpress1

--- wp-content/themes/customtheme/single.php
+++ wp-content/themes/customtheme/single.php
@@ -1,1 +1,12 @@
-Page avec contenu unique
+
+<?php get_header();?>
+<?php while(have_posts()): the_post();?>
+<article>
+    <header>
+        <h1><?php the_title(); ?></h1>
+        <div class="content">
+            <?php the_content();?>
+        </div>
+</header>
+</article>
+<?php get_footer();?>
